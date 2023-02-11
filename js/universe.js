41c625fb74954e1f7d84a74fc52d8a8174d9a8ce
lixiaomu <1377959873@qq.com>
2023-02-12 00:25:33 +0800

    Site updated: 2023-02-12 00:25:32

--- js/universe.js
+++ js/universe.js
@@ -61,14 +61,14 @@ function dark() {
 dark()
 
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
-var backimg = [
-    "url()",
-    "url()",
-    "url(https://gitee.com/youthlql/randombg/raw/master/lql_bg/00184.webp)"
-];
-//获取背景图片总数，生成随机数
-var bgindex = Math.ceil(Math.random() * (backimg.length - 1));
-//重设背景图片
-document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
-//重设banner图片
-document.getElementById("page-header").style.backgroundImage = backimg[bgindex];
+// var backimg = [
+//     "url()",
+//     "url()",
+//     "url(https://gitee.com/youthlql/randombg/raw/master/lql_bg/00184.webp)"
+// ];
+// //获取背景图片总数，生成随机数
+// var bgindex = Math.ceil(Math.random() * (backimg.length - 1));
+// //重设背景图片
+// document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
+// //重设banner图片
+// document.getElementById("page-header").style.backgroundImage = backimg[bgindex];
