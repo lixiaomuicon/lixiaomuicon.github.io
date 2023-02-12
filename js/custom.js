0ee66188c2a4dde0d7417e2594c2235ba76900c9
lixiaomu <1377959873@qq.com>
2023-02-12 20:26:44 +0800

    Site updated: 2023-02-12 20:26:42

--- js/custom.js
+++ js/custom.js
@@ -20,3 +20,22 @@ function show_date_time() {
     span_dt_dt.innerHTML = ' <font style=color:#2d85f0>' + daysold + '</font> 天 <font style=color:#f4433c>' + hrsold + '</font> 时 <font style=color:#ffbc32>' + minsold + '</font> 分 <font style=color:#0aa858>' + seconds + '</font> 秒';
 }
 show_date_time();
+
+// 返回顶部显示网页阅读进度
+window.onscroll = percent; // 执行函数
+// 页面百分比
+function percent() {
+    let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
+        b = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight) - document.documentElement.clientHeight, // 整个网页高度
+        result = Math.round(a / b * 100), // 计算百分比
+        up = document.querySelector("#go-up") // 获取按钮
+
+    if (result <= 95) {
+        up.childNodes[0].style.display = 'none'
+        up.childNodes[1].style.display = 'block'
+        up.childNodes[1].innerHTML = result;
+    } else {
+        up.childNodes[1].style.display = 'none'
+        up.childNodes[0].style.display = 'block'
+    }
+}
