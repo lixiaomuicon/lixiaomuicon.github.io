42f6fc06959f5d39f3b11aba6bd55ef9302cc627
lixiaomu <1006877324@qq.com>
2023-02-17 15:03:07 +0800

    Site updated: 2023-02-17 15:03:06

--- js/universe.js
+++ js/universe.js
@@ -189,3 +189,11 @@ function share() {
     new ClipboardJS(".share", { text: function() { return '标题：' + document.title + '\n链接：' + url } });
     btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
 }
+
+// 切换留言板热评
+function switchCommentBarrage() {
+    let flag = window.localStorage.getItem('commentBarrageDisplay') // undefined || false
+    document.getElementById('comment-barrage').style.display = flag === 'false' ? 'block' : 'none'
+        // 本地缓存一天，刷新或切换页面时仍 隐藏或显示 热评。
+    window.localStorage.setItem('commentBarrageDisplay', flag === 'false' ? 'undefined' : 'false', 86400000)
+}
