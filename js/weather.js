844703baa84e98d7b93b7b104d92b7cebf4eaab9
lixiaomu <1006877324@qq.com>
2023-03-03 15:23:38 +0800

    Site updated: 2023-03-03 15:23:37

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "20134",
+        "background": "5",
+        "tmpColor": "ffffff",
+        "tmpSize": "16",
+        "cityColor": "ffffff",
+        "citySize": "16",
+        "aqiColor": "ffffff",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "fixed": "true",
+        "vertical": "center",
+        "horizontal": "center",
+        "left": "150",
+        "top": "10",
+        "key": "91e060a32039428da24c02fff4e5fd21"
+    }
+}
