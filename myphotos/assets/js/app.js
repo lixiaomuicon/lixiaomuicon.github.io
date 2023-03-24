6ea69c09360c51d15bef939e370b4b9a9eab4e3d
lixiaomu <1006877324@qq.com>
2023-03-24 14:46:09 +0800

    Site updated: 2023-03-24 14:46:06

---
+++ myphotos/assets/js/app.js
@@ -0,0 +1,6 @@
+/* AB模版网 做最好的织梦整站模板下载网站 Www.AdminBuy.Cn */
+/* QQ：9490489 */
+/* 仿站：Fang.AdminBuy.Cn */
+/* 素材：Sc.AdminBuy.Cn */
+/* js特效：Js.AdminBuy.Cn */
+(function(a){a(".navbar-nav a").each(function(){if(this.href==window.location.href){a(this).parent().addClass("active-item");a(this).parent().parent().parent().addClass("active-item")}})})(jQuery);
