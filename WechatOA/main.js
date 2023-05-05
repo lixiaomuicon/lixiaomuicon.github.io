dfa660433052a6bd4646699c28c1fa73e87b0e40
lixiaomu <1006877324@qq.com>
2023-05-05 14:20:23 +0800

    Site updated: 2023-05-05 14:20:22

--- WechatOA/main.js
+++
@@ -1,8 +0,0 @@
-// 改进vh
-const vh = window.innerHeight * 1;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 1;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
